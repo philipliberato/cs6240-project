28252632c0434e72bc831132f9aabf3b2bd15ed3
Jonathan <jde8aa@virginia.edu>
2016-12-07 14:36:11 -0500

    added comments, avoid.txt file

--- gemini.py
+++ gemini.py
@@ -6,6 +6,7 @@ from pprint import pprint
 matches = 0
 no_matches = 0
 no_match_dict = {}
+key_error_list = []
 threshold = 0
 min_occurences = 1
 num_similarities = 5
@@ -15,11 +16,14 @@ def main():
 	global min_occurences
 	global num_similarities
 
+	#parameters to adjust for results
 	threshold = 0.5
 	min_occurences = 1
 	num_similarities = 1000
 
-   #model = Word2Vec.load("local/en_1000_no_stem/en.model")
+	#model = Word2Vec.load("local/en_1000_no_stem/en.model")
+	#model_doc is corpus of C/C++ projects' functions
+	#query_doc is current project to analyze
 	model_doc = open('documents.txt', 'r')
 	query_doc = open('linux_docs.txt','r')
    
@@ -29,56 +33,60 @@ def main():
 		seg.pop(0)
 		documents.append(seg)
 
+	#train the model on all stashed project data
 	model = Word2Vec(documents, min_count=min_occurences)
 
+	#analyze the current subject's functions calls
 	documents = []
 	for line in query_doc:
 		seg = line.lower().split()
 		callingFunc = seg.pop(0)
 		check_document(model, callingFunc, seg)
-		#documents.append(seg)
+
+	#ignore any user-specified functions from avoid.txt
+	avoid_doc = open('avoid.txt','r')
+	for line in avoid_doc:
+		seg = line.split()[0]
+		if seg in no_match_dict.keys():
+			no_match_dict.pop(seg, None)
+
 
 	print(matches)
 	print(no_matches)
 	pprint(no_match_dict)
+	print(len(key_error_list))
 
+#collect data about missing functions with close distance to
+#other present functions
 def check_document(model, callingFunc, document):
 	global threshold
 	global num_similarities
 	global matches
 	global no_matches
 	global no_match_dict
+	global key_error_list
 
+	#for a single document (caller function and its callees)
 	for token in document:
 		try:
+			#get the top N function candidates and their distances
 			candidates = model.most_similar(positive=[token],topn=num_similarities)
+			#filter out distance values, show just closely-related functions
 			candidate_strings = [str(c[0]) if c[1] > threshold else "" for c in candidates]
 			intersect = set(document).intersection(candidate_strings)
+			#if related functions not in callee functions, add to no_match dictionary
 			if len(intersect) == 0:
-				#print("No match:" + str(token) + str(candidate_strings))
 				if token not in no_match_dict.keys():
 					no_match_dict[token] = [callingFunc]
 					no_matches += 1
 				else:
 					no_match_dict[token].append(callingFunc)
 			else:
-				#print("Match:" + str(intersect))
 				matches += 1
+		#catch error when the searched key is not in the model and track
 		except KeyError as e:
-			pass #print("Exception:" + token)
+			if token not in key_error_list:
+				key_error_list.append(token) 
 			
-
-
-
-
-
-
-   #model.train(sentences)
-   #print(model.similarity('malloc', 'free'))
-   #print(sentences)
-
-
-   #print(model.similarity('woman', 'man'))
-
 if __name__ == "__main__":
     main()
